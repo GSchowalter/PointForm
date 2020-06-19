f3d2abb77e59347c162f81f7239c12ab0a84a6fd
Grant Schowalter <gschowalter@Grants-MacBook-Pro.local>
2020-06-19 15:30:43 -0400

    Initialized CommunityService.html and Fundraising.html

--- LandingPageForm.js
+++ LandingPageForm.js
@@ -1,10 +1,44 @@
 /**
  * Runs on submit of the first form.
  */
+COMMUNITYSERVICE_PAGE = 'CommunityService.html';
+FUNDRAISING_PAGE = 'Fundraising.html';
+
 const form = document.getElementById('lpform')
 form.addEventListener('submit', event => {
     // submit event detected
     event.preventDefault();
     const fname = document.getElementById('fname');
     console.log(`First name: ${fname.value}`)
+    window.sessionStorage.setItem('fname', fname.value);
+
+    const lname = document.getElementById('lname');
+    console.log(`Last name: ${lname.value}`);
+    window.sessionStorage.setItem('lname', lname.value);
+
+
+    var ele = document.getElementsByName('Event type');
+    var type = 'unchecked';
+
+    for(i = 0; i < ele.length; i++) {
+        if (ele[i].checked) {
+            console.log(`${ele[i].value} is checked`);
+            window.sessionStorage.setItem('Event type', ele[i].value);
+            type = ele[i].value;
+        }
+    }
+
+    switch(type) {
+        case 'Community Service':
+            //go to community service page
+            window.location.href = COMMUNITYSERVICE_PAGE;
+            break;
+        case 'Fundraising':
+            //go to fundraising page
+            window.location.href = FUNDRAISING_PAGE;
+            break;
+        default:
+            //reload the page
+    }
 });
+
